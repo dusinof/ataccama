f40a4c3e261b66968fc8bed612e640fac58158a9
dusinof <dusinof@gmail.com>
2021-08-25 19:48:33 +0200

    Main skeleton

---
+++ src/main/java/com/berger/ataccama/browserdb/browserdb/BrowserdbApplication.java
@@ -0,0 +1,13 @@
+package com.berger.ataccama.browserdb.browserdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowserdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berger/ataccama/browserdb/browserdb/BrowserdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berger.ataccama.browserdb.browserdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
